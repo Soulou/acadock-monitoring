42d3c612791825077fc364ed66484930e0931c04
Soulou <leo@unbekandt.eu>
2013-05-26 16:00:36 +0200

    Go fmt on the project

--- lxc/cpu/cpu.go
+++ lxc/cpu/cpu.go
@@ -1,67 +1,72 @@
 package cpu
 
 import (
-  "Acadock/lxc"
-  "fmt"
-  "log"
-  "os"
-  "runtime"
-  "strconv"
-  "time"
+	"Acadock/lxc"
+	"fmt"
+	"log"
+	"os"
+	"runtime"
+	"strconv"
+	"time"
 )
 
 const (
 	LXC_CPUACCT_DIR        = "/sys/fs/cgroup/cpuacct/lxc"
 	LXC_CPUACCT_USAGE_FILE = "cpuacct.usage"
-  REFRESH_TIME           = 1
+	REFRESH_TIME           = 1
 )
 
 var (
-  previousCpuUsages = make(map[string]int64)
-  cpuUsages = make(map[string]int64)
+	previousCpuUsages = make(map[string]int64)
+	cpuUsages         = make(map[string]int64)
 )
 
+func cpuacctUsage(container string) (int64, error) {
+	file := fmt.Sprintf("%s/%s/%s", LXC_CPUACCT_DIR, container, LXC_CPUACCT_USAGE_FILE)
+	f, err := os.Open(file)
+	if err != nil {
+		log.Println(err)
+		return 0, err
+	}
 
-func cpuacctUsage(container string) int64 {
-  file := fmt.Sprintf("%s/%s/%s", LXC_CPUACCT_DIR, container, LXC_CPUACCT_USAGE_FILE)
-  f, err := os.Open(file)
-  if err != nil {
-    log.Fatalln(err)
-  }
+	buffer := make([]byte, 16)
+	n, err := f.Read(buffer)
+	buffer = buffer[:n]
 
-  buffer := make([]byte, 16)
-  n, err := f.Read(buffer)
-  buffer = buffer[:n]
+	bufferStr := string(buffer)
+	bufferStr = bufferStr[:len(bufferStr)-1]
 
-  bufferStr := string(buffer)
-  bufferStr = bufferStr[:len(bufferStr)-1]
-
-  res, err := strconv.ParseInt(bufferStr, 10, strconv.IntSize)
-  if err != nil {
-    log.Fatalln("Fail to parse : ", err)
-  }
-  return res
+	res, err := strconv.ParseInt(bufferStr, 10, strconv.IntSize)
+	if err != nil {
+		log.Println("Failed to parse : ", err)
+		return 0, err
+	}
+	return res, nil
 }
 
 func Monitor() {
-  tick := time.NewTicker(REFRESH_TIME * time.Second)
-  for {
-    <-tick.C
-    for k, v := range cpuUsages {
-       previousCpuUsages[k] = v
-    }
+	tick := time.NewTicker(REFRESH_TIME * time.Second)
+	for {
+		<-tick.C
+		for k, v := range cpuUsages {
+			previousCpuUsages[k] = v
+		}
 
-    containers, err := lxc.ListContainer()
-    if err != nil {
-      log.Fatalln(err)
-    }
+		containers, err := lxc.ListContainer()
+		if err != nil {
+			log.Fatalln(err)
+		}
 
-    for _, container := range containers {
-      cpuUsages[container] = cpuacctUsage(container)
-    }
-  }
+		for _, container := range containers {
+			n, err := cpuacctUsage(container)
+			if err != nil {
+				log.Fatalln(err)
+			}
+			cpuUsages[container] = n
+		}
+	}
 }
 
 func GetUsage(name string) int64 {
-  return int64((float64((cpuUsages[name] - previousCpuUsages[name])) / float64(1e9) / float64(runtime.NumCPU())) * 100)
+	return int64((float64((cpuUsages[name] - previousCpuUsages[name])) / float64(1e9) / float64(runtime.NumCPU())) * 100)
 }
--- lxc/mem/mem.go
+++ lxc/mem/mem.go
@@ -1,39 +1,39 @@
 package mem
 
 import (
-  "fmt"
-  "log"
-  "os"
-  "strconv"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
 )
 
 const (
-	LXC_MEM_DIR            = "/sys/fs/cgroup/memory/lxc"
-	LXC_MEM_USAGE_FILE     = "memory.usage_in_bytes"
+	LXC_MEM_DIR        = "/sys/fs/cgroup/memory/lxc"
+	LXC_MEM_USAGE_FILE = "memory.usage_in_bytes"
 )
 
 func GetUsage(name string) (int64, error) {
 	path := fmt.Sprintf("%s/%s/%s", LXC_MEM_DIR, name, LXC_MEM_USAGE_FILE)
 	f, err := os.Open(path)
-  defer f.Close()
 	if err != nil {
 		log.Println("Error while opening : ", err)
 		return 0, err
 	}
+	defer f.Close()
 
-  buffer := make([]byte, 16)
-  n, err := f.Read(buffer)
-  if err != nil {
-    log.Println("Error while reading ", path, " : ", err)
-    return 0, err
-  }
+	buffer := make([]byte, 16)
+	n, err := f.Read(buffer)
+	if err != nil {
+		log.Println("Error while reading ", path, " : ", err)
+		return 0, err
+	}
 
-  buffer = buffer[:n-1]
-  val, err := strconv.ParseInt(string(buffer), 10, strconv.IntSize)
-  if err != nil {
-    log.Println("Error while parsing ", string(buffer), " : ", err)
-    return 0, err
-  }
+	buffer = buffer[:n-1]
+	val, err := strconv.ParseInt(string(buffer), 10, strconv.IntSize)
+	if err != nil {
+		log.Println("Error while parsing ", string(buffer), " : ", err)
+		return 0, err
+	}
 
-  return val, nil
+	return val, nil
 }
